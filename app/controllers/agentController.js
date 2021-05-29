f8e3ba1df5c1ab32ff020357926b850a96864c3f
Abdelhadi <abdo.sabani30@gmail.com>
2021-05-29 23:35:12 +0100

    Add sign-in with agent controller

---
+++ app/controllers/agentController.js
@@ -0,0 +1,30 @@
+const { Agent } = require("../models");
+const createError = require("http-errors");
+const { agentValidator } = require("../helpers/validationSchema");
+const { signAccessToken } = require("../helpers/jwt");
+
+const signIn = async (req, res, next) => {
+    try {
+        let data = await agentValidator(req.body, { username: 1, password: 1 });
+        let agent = await Agent.findOne({
+            where: {
+                username: data.username,
+            },
+        });
+        if (!agent) {
+            throw createError.NotFound("Agent not found !");
+        } else if (!agent.checkPassword(data.password)) {
+            throw createError.Unauthorized("username/password incorrect");
+        }
+        const accessToken = await signAccessToken(data.username);
+        res.json({
+            accessToken,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = {
+    signIn,
+};
