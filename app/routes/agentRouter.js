6c9da46af453aa4cbac43b15ddf7c46fc864a8bb
Abdelhadi <abdo.sabani30@gmail.com>
2021-06-03 15:04:14 +0100

    Remove update agent router

--- app/routes/agentRouter.js
+++ app/routes/agentRouter.js
@@ -4,7 +4,6 @@ const protectRouter = require("../middlewares/protectRouter");
 
 router.post("/sign-in", agentController.signIn);
 router.post("/sign-up", protectRouter, agentController.signUp);
-router.put("/agents", protectRouter, agentController.updateAgent);
 router.delete("/agents", protectRouter, agentController.deleteAgent);
 router.get("/agents", protectRouter, agentController.getAgents);
 router.put(
