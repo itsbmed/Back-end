76e51e9cc278111b72a9c35fc9baeedd064437c7
Abdelhadi <abdo.sabani30@gmail.com>
2021-06-03 13:58:39 +0100

    Add update agent controller

--- app/controllers/agentController.js
+++ app/controllers/agentController.js
@@ -55,7 +55,29 @@ const signUp = async (req, res, next) => {
     }
 };
 
+const updateAgent = async (req, res, next) => {
+    try {
+        let currentAgent = req.currentAgent;
+        if (!currentAgent.isAdmin) throw createError.Forbidden();
+        let data = await agentValidator(req.body, {
+            userName: 2,
+            passWord: 2,
+            firstName: 2,
+            lastName: 2,
+            isAdmin: 2,
+        });
+        let newData = { ...currentAgent.dataValues, ...data };
+        await currentAgent.updateAgent(newData);
+        if (data.passWord) await currentAgent.hashPassword();
+        await currentAgent.save();
+        res.json(currentAgent);
+    } catch (err) {
+        next(err);
+    }
+};
+
 module.exports = {
     signIn,
     signUp,
+    updateAgent,
 };
