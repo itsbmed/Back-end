5697f00fc6e3bcc88a5ba93efd6abe06202ddb54
Abdelhadi <abdo.sabani30@gmail.com>
2021-06-04 00:31:24 +0100

    Add bill model

---
+++ app/models/billModel.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class Bill extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Bill.init(
+        {
+            nReceipt: DataTypes.INTEGER,
+            nBill: DataTypes.INTEGER,
+            nature: DataTypes.STRING,
+            episodeId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: "Episodes",
+                    key: "id",
+                },
+            },
+            medicament: DataTypes.INTEGER,
+            actes: DataTypes.INTEGER,
+            total: DataTypes.INTEGER,
+            category: DataTypes.ENUM("PAID", "RAMED"),
+            prosthesis: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            timestamps: false,
+            modelName: "Bill",
+        }
+    );
+    return Bill;
+};
