30fa3bc2ab8a9855052f1309d94c2a11dcd7afc8
Abdelhadi <abdo.sabani30@gmail.com>
2021-06-03 14:28:29 +0100

    Add get agents router

--- app/routes/agentRouter.js
+++ app/routes/agentRouter.js
@@ -6,4 +6,5 @@ router.post("/sign-in", agentController.signIn);
 router.post("/sign-up", protectRouter, agentController.signUp);
 router.put("/agents", protectRouter, agentController.updateAgent);
 router.delete("/agents", protectRouter, agentController.deleteAgent);
+router.get("/agents", protectRouter, agentController.getAgents);
 module.exports = router;
