e6ef144d30f0fd864534c6c4d5667e3de65b9a97
Abdelhadi <abdo.sabani30@gmail.com>
2021-06-03 14:13:52 +0100

    Add delete agent router

--- app/routes/agentRouter.js
+++ app/routes/agentRouter.js
@@ -5,4 +5,5 @@ const protectRouter = require("../middlewares/protectRouter");
 router.post("/sign-in", agentController.signIn);
 router.post("/sign-up", protectRouter, agentController.signUp);
 router.put("/agents", protectRouter, agentController.updateAgent);
+router.delete("/agents", protectRouter, agentController.deleteAgent);
 module.exports = router;
